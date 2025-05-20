2046bb400c1f30a061e09631e05bf0383731003e
ManishKumarRai-creator <98271764+kumarmanishrai@users.noreply.github.com>
2025-05-20 22:30:18 +0530

    changed some gitignore entries

--- server/dist/route/userRouter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const userController_1 = require("../controller/userController");
-const asyncHandler = (fn) => (req, res, next) => Promise.resolve(fn(req, res)).catch(next);
-const UserRouter = (app) => {
-    app.get('/track-visitor', asyncHandler(userController_1.trackVisitor));
-    app.post('/track-interaction', asyncHandler(userController_1.trackInteraction));
-};
-exports.default = UserRouter;
