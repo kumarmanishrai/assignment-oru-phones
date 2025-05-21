56b9609d9c0b7c3ec64da744a4fd63b4b7050b6c
ManishKumarRai-creator <98271764+kumarmanishrai@users.noreply.github.com>
2025-05-22 00:56:58 +0530

    changed redis

--- server/src/index.ts
+++ server/src/index.ts
@@ -42,8 +42,9 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: true,
+      secure: process.env.NODE_ENV === 'production',
       httpOnly: true,
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       maxAge: 1000 * 60 * 60, // 1 hour
     },
   })
